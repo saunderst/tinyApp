885329c9e77aadd00e2e7f8810c0a2cd805baaa8
saunderst <thomas130@gmx.com>
2018-03-21 19:57:15 -0400

    Cookie-based login/logout

--- express-server.js
+++ express-server.js
@@ -1,15 +1,17 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+const cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 8080; // default port 8080
+
 
 // for first pass at tinyURL generation
 function generateRandomString() {
   let rstring = "";
   let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
+  
   for (let i = 0; i < 6; i++)
-    rstring += chars[Math.floor(Math.random() * chars.length)];
+  rstring += chars[Math.floor(Math.random() * chars.length)];
   return rstring;
 }
 
@@ -17,6 +19,8 @@ app.set("view engine", "ejs");
 
 // enable us to use local resources for display
 app.use("/public", express.static('public'));
+// for eyeing people's cookies
+app.use(cookieParser())
 
 // a couple of predefined URL mappings
 var urlDatabase = {
@@ -34,7 +38,10 @@ app.get("/", (req, res) => {
 
 // display list of existing URL mappings
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -70,12 +77,20 @@ app.post("/urls/:id/update", (req, res) => {
 
 // be sure this goes before the generic /urls/:id
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"],
+  };
+  
+  res.render("urls_new", templateVars);
 });
 
 // show the requested URL mapping
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, urls: urlDatabase };
+  let templateVars = {
+    username: req.cookies["username"],
+    shortURL: req.params.id,
+    urls: urlDatabase
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -86,3 +101,16 @@ app.get("/u/:shortURL", (req, res) => {
   else
     res.redirect(longURL);
 });
+
+// login request: set cookie
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('http://localhost:8080/urls');
+});
+
+// logout request: delete cookie
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('http://localhost:8080/urls');
+});
+
