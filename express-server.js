9892b0e3bf05f4100b4df537ada4ac4475082ee2
saunderst <thomas130@gmx.com>
2018-03-22 16:23:13 -0400

    user registration complete

--- express-server.js
+++ express-server.js
@@ -1,75 +1,75 @@
-const bodyParser = require("body-parser");
-const express = require("express");
+const bodyParser = require('body-parser');
+const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 8080; // default port 8080
 const users = { 
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
+  'userRandomID': {
+    id: 'userRandomID', 
+    email: 'user@example.com', 
+    password: 'purple-monkey-dinosaur'
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
+ 'user2RandomID': {
+    id: 'user2RandomID', 
+    email: 'user2@example.com', 
+    password: 'dishwasher-funk'
   }
 };
 
 // for first pass at tinyURL generation
 function generateRandomString() {
-  let rstring = "";
-  let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let rstring = '';
+  let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
   
   for (let i = 0; i < 6; i++)
   rstring += chars[Math.floor(Math.random() * chars.length)];
   return rstring;
 }
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // enable us to use local resources for display
-app.use("/public", express.static('public'));
+app.use('/public', express.static('public'));
 // for eyeing people's cookies
 app.use(cookieParser())
 
 // a couple of predefined URL mappings
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 };
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/", (req, res) => {
-  res.end("Hello!");    // just being friendly, maybe a bit loud
+app.get('/', (req, res) => {
+  res.end('Hello!');    // just being friendly, maybe a bit loud
 });
 
 // display list of existing URL mappings
-app.get("/urls", (req, res) => {
+app.get('/urls', (req, res) => {
   let templateVars = {
-    user: users[req.cookies["user.id"]],
+    user: users[req.cookies['user_id']],
     urls: urlDatabase
   };
-  res.render("urls_index", templateVars);
+  res.render('urls_index', templateVars);
 });
 
 // need this for handling form submission
 app.use(bodyParser.urlencoded({extended: true}));
 
 // handle request for new URL mapping
-app.post("/urls", (req, res) => {
+app.post('/urls', (req, res) => {
   let shortURL = generateRandomString();
-  if (req.body.longURL.substring(0,4) !== "http")
-    req.body.longURL = "http://" + req.body.longURL;
+  if (req.body.longURL.substring(0,4) !== 'http')
+    req.body.longURL = 'http://' + req.body.longURL;
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect('http://localhost:8080/urls/' + shortURL);
 });
 
 // handle request to delete existing URL mapping
-app.post("/urls/:id/delete", (req, res) => {
+app.post('/urls/:id/delete', (req, res) => {
   if (!urlDatabase[req.params.id])
     res.redirect('http://localhost:8080/urls');
   else {
@@ -79,33 +79,33 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 // handle request to alter existing URL mapping
-app.post("/urls/:id/update", (req, res) => {
-  if (req.body.longURL.substring(0,4) !== "http")
-    req.body.longURL = "http://" + req.body.longURL;
+app.post('/urls/:id/update', (req, res) => {
+  if (req.body.longURL.substring(0,4) !== 'http')
+    req.body.longURL = 'http://' + req.body.longURL;
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect('http://localhost:8080/urls');
 });
 
 // be sure this goes before the generic /urls/:id
-app.get("/urls/new", (req, res) => {
+app.get('/urls/new', (req, res) => {
   let templateVars = {
-    user: users[req.cookies["user.id"]]
+    user: users[req.cookies['user.id']]
   };
   
-  res.render("urls_new", templateVars);
+  res.render('urls_new', templateVars);
 });
 
 // show the requested URL mapping
-app.get("/urls/:id", (req, res) => {
+app.get('/urls/:id', (req, res) => {
   let templateVars = {
-    user: users[req.cookies["user.id"]],
+    user: users[req.cookies['user.id']],
     shortURL: req.params.id,
     urls: urlDatabase
   };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get('/u/:shortURL', (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   if (!longURL)
     res.redirect('http://localhost:8080/urls');
@@ -114,31 +114,41 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 // login request: set cookie
-app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('http://localhost:8080/urls');
+app.post('/login', (req, res) => {
+  let userFound = false;
+  debugger
+  for (let user in users) {
+    if (users[user].email === req.body.email && users[user].password === req.body.password) {
+      userFound = true;
+      res.cookie('user_id', users[user].id);
+      res.redirect('http://localhost:8080/urls');
+      break;
+    }
+  }
+  if (!userFound) {
+    res.status(403).send('I don\'t even know who you are.');
+  }
 });
 
 // logout request: delete cookie
-app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+app.get('/logout', (req, res) => {
+  res.clearCookie('user_id');
   res.redirect('http://localhost:8080/urls');
 });
 
 // display the registration form
-app.get("/register", (req, res) => {
-  res.render("register");
+app.get('/register', (req, res) => {
+  res.render('register');
 });
 
 // handle submission from  the registration form
-app.post("/register", (req, res) => {
-  debugger
-  if (req.body.email === "" ||req.body.password === "") {
-    res.status(400).send("We're gonna need values for email and password.");
+app.post('/register', (req, res) => {
+  if (req.body.email === '' ||req.body.password === '') {
+    res.status(400).send('We\'re gonna need values for email and password.');
   } else {
     for (let user in users) {
       if (users[user].email === req.body.email) {
-        res.status(400).send("Don't use somebody else's email.  Please.");
+        res.status(400).send('Don\'t use somebody else\'s email.  Please.');
         return;
       }
     }
@@ -154,6 +164,6 @@ app.post("/register", (req, res) => {
 });
 
 // display the login page
-app.get("/login", (req, res) => {
-  res.render("login");
+app.get('/login', (req, res) => {
+  res.render('login');
 });
